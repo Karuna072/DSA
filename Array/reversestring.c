783b9c90339fb8522a05ba224d7024a4f361a5f0
Karuna072 <87273699+Karuna072@users.noreply.github.com>
2021-09-19 12:55:46 +0530

    Update and rename reverse-string to reversestring.c

---
+++ Array/reversestring.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	int len=0;
+	char temp;
+	char str[100];
+	
+	scanf("%s",&str);
+	while(str[len]!='\0')
+	{
+		len++;
+	}
+	for(int i=0;i<(len-1)/2;i++)
+	{
+		temp=str[i];
+		str[i]=str[len-1-i];
+		str[len-1-i]=temp;
+	}
+	printf("%s",str);
+	// your code goes here
+	return 0;
+}
+
