22fb09f6310f4f376b58e200c89c5dff57f1b092
Karuna Naik <87273699+Karuna072@users.noreply.github.com>
2021-09-20 20:56:37 +0530

    Create sort012.c

---
+++ Array/sort012.c
@@ -0,0 +1,26 @@
+//in c
+void sort012(int a[], int n)
+{
+    int temp;
+   
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+            
+        }
+        
+    }
+    
+    
+}
+
+//in java 
+//Arrays.sort(a);
+ 
